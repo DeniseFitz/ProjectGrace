524237cc07ceb35b84811386781890eda7d975fd
DeniseFitz <Denise.Poll3@gmail.com>
2022-07-05 01:23:24 +0000

    comment out for now

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-print('main I am, with Grace')
+'''
+import requests
+import pandas as pd
+import sqlalchemy as db
 
-def fun1():
- print('incorrect indent')
+
+#creating data frame to add data to
+col_names = ['Title', 'Author', 'Link']
+df  = pd.DataFrame(columns = col_names)
+
+engine = db.create_engine('sqlite:///hackernews.db')
+df.to_sql('stories', con=engine, if_exists='replace', index=False)
+print(df)
+'''
+print('in main.py') 
